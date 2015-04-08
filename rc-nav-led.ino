aeb0ee2fc3bf1641142509d3efecb4e4c8e9e462
illuxions <illuxion@gmail.com>
2015-04-07 22:23:26 -0700

    first update after fork to support nopixels

--- rc-nav-led.ino
+++ rc-nav-led.ino
@@ -12,27 +12,47 @@
  * @version 0.3
  * @date 2014-06-12
  * 
+ * brutally masacred for Neopixel use by Illuxions 2015-4-7
+ * You MUST customize this for the number of pixels and pixel positions you will use
  */
 
+#include <Adafruit_NeoPixel.h>  //adafruit neopixel library
+
+#define PIXELPIN 2  // neopixel control pin
+#define PIXELNUM 13 //total number of pixels
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(PIXELNUM, PIXELPIN, NEO_GRB + NEO_KHZ800);
+
 // Landing light settings
-#define LL_IRQ_NUMBER 0 // Interrupt number to use (0 = pin 2 on most boards)
-#define LL_PIN_SERVO 2 // Servo input pin number - this needs to match whatever interrupt is used
-#define LL_PIN_LIGHT 3 // Landing light output pin number
+const byte LPIXNUM = 3;  //number of landing pixels
+const byte LANDPIX[LPIXNUM] = {5,6,7}; //landing pixel positions
+#define LL_IRQ_NUMBER 3 // Interrupt number to use (0 = pin 2 on most boards)
+#define LL_PIN_SERVO 3 // Servo input pin number - this needs to match whatever interrupt is used
 #define LL_SERVO_THRESHOLD 1500 // Servo signal threshold to turn on/off landing light (pulse width in microseconds, 1000 to 2000)
 #define LL_SERVO_DEAD_ZONE 100 // Servo signal dead-zone size, eliminates flicker
 #define LL_SERVO_REVERSED true   // Whether or not the servo channel is reversed
 
 // Strobe settings
-#define STB_PIN_LIGHT 4 // Pin number for strobe light output
-#define STB_BLINK_INTERVAL 2000000 // Blink interval for strobe light in microseconds
+const byte SPIXNUM = 2; //number of strobe pixels
+const byte STROBEPIX[SPIXNUM] = {0,12}; //strobe pixel positions
+#define STB_BLINK_INTERVAL 2000000//2000000 // Blink interval for strobe light in microseconds
 
 // Anti-collision beacon settings
-#define ACB1_PIN_LIGHT 0 // Pin number for anti-collision beacon 1
-#define ACB2_PIN_LIGHT 1 // Pin number for anti-collision beacon 2
-#define ACB_FADE_MIN 10 // Minimum fade level for beacon (0-255)
-#define ACB_FADE_MAX 50 // Maximum fade level for beacon (0-255)
-#define ACB_FADE_INTERVAL 30000 // Fade step interval, in microseconds (lower numbers = faster fade)
-
+const byte APIXNUM = 4; //number of for anti-collision beacon 1 pixels
+byte FADEAPIX[APIXNUM] = {1,2,3,4}; //for anti-collision beacon 1 pixel positions
+const byte BPIXNUM = 4; //; //number of for anti-collision beacon 2 pixels
+byte FADEBPIX[BPIXNUM] = {8,9,10,11}; //for anti-collision beacon 2 pixel positions
+#define ACB_FADE_MIN 30 // Minimum fade level for beacon (0-255)
+#define ACB_FADE_MAX 70 // Maximum fade level for beacon (0-255)
+#define ACB_FADE_STR 255
+#define ACB_FADE_INTERVAL 25000//30000 // Fade step interval, in microseconds (lower numbers = faster fade)
 
 // Var declarations
 volatile unsigned long servoPulseStartTime;
@@ -44,17 +64,30 @@ unsigned long lastStrobeTime = 0;
 int currentFade = ACB_FADE_MIN;
 int fadeDirection = 1;
 
+
+
 // Called on power on or reset
 void setup()
 {
+  uint16_t i;
+  pixels.begin();
+
+
+  for (i = 0; i < pixels.numPixels(); i++) {  //black all pixels
+    pixels.setPixelColor(i, pixels.Color(0, 0, 0));
+  }
+  
+  
+ //If you want Static Pixels, here's a good place to put them Green, Red, Blue 
+ // pixels.setPixelColor(2, pixels.Color(50,0,0));
+ // pixels.setPixelColor(3, pixels.Color(50,0,0));
+ // pixels.setPixelColor(9, pixels.Color(0,50,0));
+ // pixels.setPixelColor(10, pixels.Color(0,50,0));
+  pixels.show();
+
   // Set up interrupt handler
   attachInterrupt(LL_IRQ_NUMBER, measureServoSignal, CHANGE);
 
-  // Declare output pins
-  pinMode(LL_PIN_LIGHT, OUTPUT);
-  pinMode(STB_PIN_LIGHT, OUTPUT);
-  pinMode(ACB1_PIN_LIGHT, OUTPUT);
-  pinMode(ACB2_PIN_LIGHT, OUTPUT);
 }
 
 // Called continuouly
@@ -64,7 +97,7 @@ void loop()
 
   // Check if the landing light should be turned on
   checkLandingLight();
-  
+
   // Check if it's time to fade the anti-collision lights
   if ((currentTime - lastFadeTime) > ACB_FADE_INTERVAL) {
     doFade();
@@ -74,7 +107,7 @@ void loop()
   // Check if it's time to blink the strobes
   if ((currentTime - lastStrobeTime) > STB_BLINK_INTERVAL) {
     doStrobe();
-    lastStrobeTime = currentTime; 
+    lastStrobeTime = currentTime;
   }
 }
 
@@ -95,18 +128,30 @@ void checkLandingLight()
   if (servoPulseWidth >= threshold) {
     setLandingLight(true);
   } else {
-    setLandingLight(false);
+    setLandingLight(true); //return to false after debug
   }
 }
 
 // Turn on or off landing light
 void setLandingLight(boolean state)
 {
+  uint16_t j;
   float i;
   if (state && !curLandingLight) {
-    digitalWrite(LL_PIN_LIGHT, HIGH);
+    for (j = 0; j < LPIXNUM; j++) {
+
+      pixels.setPixelColor(LANDPIX[j], pixels.Color(200, 200, 200)); //landing lights set near full power
+    }
+    pixels.show();
+    //digitalWrite(LL_PIN_LIGHT, HIGH);
   } else if (!state && curLandingLight) {
-    digitalWrite(LL_PIN_LIGHT, LOW);
+    for (j = 0; j < LPIXNUM; j++) {
+
+      pixels.setPixelColor(LANDPIX[j], pixels.Color(0, 0, 0));  //turn off landing lights
+    }
+
+    //digitalWrite(LL_PIN_LIGHT, LOW);
+
   }
   curLandingLight = state;
 }
@@ -114,40 +159,68 @@ void setLandingLight(boolean state)
 // Fade anti-collision LEDs
 void doFade()
 {
+  uint16_t j, k, l;
   currentFade += fadeDirection;
   if (currentFade == ACB_FADE_MAX || currentFade == ACB_FADE_MIN) {
     // If we hit the fade limit, flash the high beacon, and flip the fade direction
     if (fadeDirection == 1) {
-      analogWrite(ACB1_PIN_LIGHT, 255);
-
+          for (j = 0; j < APIXNUM; j++) {  //loop through all enabled pins GRB
+        pixels.setPixelColor(FADEAPIX[j], pixels.Color(ACB_FADE_STR, 0, 0));
+      }
+      pixels.show();
     } else {
-      analogWrite(ACB2_PIN_LIGHT, 255);
+      for (j = 0; j < APIXNUM; j++) { //loop through all enabled pins GRB
+        pixels.setPixelColor(FADEBPIX[j], pixels.Color(0, ACB_FADE_STR, 0));
+      }
+      pixels.show();
     }
-    delay(50); 
-    fadeDirection *= -1; 
+    delay(50);
+    fadeDirection *= -1;
   }
 
-  analogWrite(ACB1_PIN_LIGHT, currentFade);
-  analogWrite(ACB2_PIN_LIGHT, ACB_FADE_MAX - currentFade + ACB_FADE_MIN);
+
+  for (k = 0; k < APIXNUM; k++) { //loop through all enabled pins  GRB
+    pixels.setPixelColor(FADEAPIX[k], pixels.Color(currentFade, 0, 0));
+  }
+  for (l = 0; l < APIXNUM; l++) { //loop through all enabled pins GRB
+    pixels.setPixelColor(FADEBPIX[l], pixels.Color(0, ACB_FADE_MAX - currentFade + ACB_FADE_MIN, 0));
+  }
+  pixels.show();
 }
 
 // Strobe double-blink
 void doStrobe()
 {
-  digitalWrite(STB_PIN_LIGHT, HIGH);
+  uint16_t j;
+  for (j = 0; j < SPIXNUM; j++) { //loop through all enabled Strobe pins on GRB
+    pixels.setPixelColor(STROBEPIX[j], pixels.Color(255, 255, 255));
+  }
+  pixels.show();
+  //digitalWrite(STB_PIN_LIGHT, HIGH);
   delay(50);
-  digitalWrite(STB_PIN_LIGHT, LOW);
+  for (j = 0; j < SPIXNUM; j++) { //loop through all enabled Strobe pins off GRB
+    pixels.setPixelColor(STROBEPIX[j], pixels.Color(0, 0, 0));
+  }
+  pixels.show();
+  // digitalWrite(STB_PIN_LIGHT, LOW);
   delay(50);
-  digitalWrite(STB_PIN_LIGHT, HIGH);
+  for (j = 0; j < SPIXNUM; j++) { //loop through all enabled Strobe pins on GRB
+    pixels.setPixelColor(STROBEPIX[j], pixels.Color(255, 255, 255));
+  }
+  pixels.show();
+  // digitalWrite(STB_PIN_LIGHT, HIGH);
   delay(50);
-  digitalWrite(STB_PIN_LIGHT, LOW);
+  for (j = 0; j < SPIXNUM; j++) { //loop through all enabled Strobe pins off GRB
+    pixels.setPixelColor(STROBEPIX[j], pixels.Color(0, 0, 0));
+  }
+  pixels.show();
 }
 
 // Measure servo PWM signal
 void measureServoSignal()
 {
   int pinState = digitalRead(LL_PIN_SERVO);
-  if(pinState == HIGH) { 
+  if (pinState == HIGH) {
     // Beginning of PWM pulse, mark time
     servoPulseStartTime = micros();
   } else {
