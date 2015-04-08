33aa1e93b47f768121d57f9430ae517c436c8b5a
illuxions <illuxions@users.noreply.github.com>
2015-04-08 15:49:22 -0700

    Update rc-nav-led.ino

--- rc-nav-led.ino
+++ rc-nav-led.ino
@@ -15,53 +15,59 @@
  * brutally masacred for Neopixel use by Illuxions 2015-4-7
  * You MUST customize this for the number of pixels and pixel positions you will use
  */
+// Var declarations
 #include <Adafruit_NeoPixel.h>  //adafruit neopixel library
-
-
 #define PIXELPIN 1  // neopixel control pin
 #define PIXELNUM 13 //total number of pixels
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXELNUM, PIXELPIN, NEO_GRB + NEO_KHZ800);
 
+
+uint32_t cBlack = strip.Color(0,0,0);
+uint32_t cWhite = strip.Color(255,255,255);
+uint32_t cRed = strip.Color(0,100,0);
+uint32_t cGreen = strip.Color(100,0,0);
+uint32_t cBlue = strip.Color(0,0,100);
+
 // Landing light settings
-const byte LPIXNUM = 3;  //number of landing pixels
-const byte LANDPIX[LPIXNUM] = {5, 6, 7}; //landing pixel positions
-#define LL_IRQ_NUMBER 0 // Interrupt number to use (0 = pin 2 on most boards)
-#define LL_PIN_SERVO 0 // Servo input pin number - this needs to match whatever interrupt is used
+const byte LPIXNUM = 1;  //number of landing pixels
+const byte LANDPIX[LPIXNUM] = {6}; //landing pixel positions
+#define LL_IRQ_NUMBER 5 // Interrupt number to use (0 = pin 2 on most boards)
+#define LL_PIN_SERVO 5 // Servo input pin number - this needs to match whatever interrupt is used
 #define LL_SERVO_THRESHOLD 1500 // Servo signal threshold to turn on/off landing light (pulse width in microseconds, 1000 to 2000)
 #define LL_SERVO_DEAD_ZONE 100 // Servo signal dead-zone size, eliminates flicker
 #define LL_SERVO_REVERSED true   // Whether or not the servo channel is reversed
-uint8_t LL_COLOR[] = {255, 255, 255}; //landing light color and brightness GRB
+uint32_t LL_COLOR = strip.Color(175,175,175); //landing light color and brightness GRB
 
 
 // Strobe settings 
-const byte SPIXNUM = 10; //number of strobe pixels
-const byte STROBEPIX[SPIXNUM] = {0, 1, 2, 3, 4, 8, 9, 10, 11, 12}; //strobe pixel positions
-#define STB_BLINK_INTERVAL 3000000 // Blink interval for strobe light in microseconds
-uint8_t STB_C[] = {255, 255, 255}; //strobe color
-uint8_t STB_TIMES = 2; //number of flashes per strobe cycle
+const byte SPIXNUM = 13; //number of strobe pixels
+const byte STROBEPIX[SPIXNUM] = {0, 1, 2, 3, 4,5,6,7, 8, 9, 10, 11, 12}; //strobe pixel positions
+#define STB_BLINK_INTERVAL 4000000 // Blink interval for strobe light in microseconds
+uint32_t STB_COLOR = strip.Color(100,100,100); //strobe color
+uint8_t STB_TIMES = 3; //number of flashes per strobe cycle
+uint8_t STB_DEL = 25; //ms pause for strobe
 
 
-                  // Anti-collision beacon settings
-const byte APIXNUM = 2; //number of for anti-collision beacon 1 pixels.  *!!  Must have samequantity in each of 4 sets
-const byte FADEAPIX[APIXNUM] = {1, 4}; //for anti-collision beacon 1 pixel positions
-const byte FADEBPIX[APIXNUM] = {8, 11}; //for anti-collision beacon 2 pixel positions
-const byte FADECPIX[APIXNUM] = {9, 10}; //for anti-collision beacon 1 pixel positions
-const byte FADEDPIX[APIXNUM] = {2, 3}; //for anti-collision beacon 2 pixel positions
+// Anti-collision beacon settings
+const byte APIXNUM = 3; //number of for anti-collision beacon 1 pixels.  *!!  Must have samequantity in each of 4 sets
+const byte FADEAPIX[APIXNUM] = {0,1, 4}; //for anti-collision beacon 1 pixel positions
+const byte FADEBPIX[APIXNUM] = {7, 8, 11}; //for anti-collision beacon 2 pixel positions
+const byte FADECPIX[APIXNUM] = {9, 10,12}; //for anti-collision beacon 1 pixel positions
+const byte FADEDPIX[APIXNUM] = {2, 3,5}; //for anti-collision beacon 2 pixel positions
 
 #define ACB_FADE_MIN 10 // Minimum fade level for beacon (0-255)
 #define ACB_FADE_MAX 75 // Maximum fade level for beacon (0-255)
 #define ACB_FADE_STR 255
-#define ACB_FADE_INTERVAL 11000 // Fade step interval, in microseconds (lower numbers = faster fade)
+#define ACB_FADE_INTERVAL 18000 // Fade step interval, in microseconds (lower numbers = faster fade)
 
-// Var declarations
 volatile unsigned long servoPulseStartTime;
 volatile int servoPulseWidth = 0;
 boolean curLandingLight = false;
-
 unsigned long lastFadeTime = 0;
 unsigned long lastStrobeTime = 0;
 int currentFade = ACB_FADE_MIN;
 int fadeDirection = 1;
+
 
 // Called on power on or reset
 void setup()
@@ -132,14 +138,14 @@ void setLandingLight(boolean state)
   if (state && !curLandingLight) {
     for (j = 0; j < LPIXNUM; j++) {
 
-      strip.setPixelColor(LANDPIX[j], strip.Color(LL_COLOR[0], LL_COLOR[1], LL_COLOR[2])); //landing lights by LL_C
+      strip.setPixelColor(LANDPIX[j], LL_COLOR); //landing lights by LL_C
     }
     strip.show();
     
   } else if (!state && curLandingLight) {
     for (j = 0; j < LPIXNUM; j++) {
 
-      strip.setPixelColor(LANDPIX[j], strip.Color(0, 0, 0));  //turn off landing lights
+      strip.setPixelColor(LANDPIX[j], cBlack);  //turn off landing lights
     }
 
 
@@ -198,16 +204,16 @@ void doStrobe()
     b[k] = (strip.getPixelColor(k)      );
     }
     for (j = 0; j < SPIXNUM; j++) { //loop through all enabled Strobe pins on GRB
-      strip.setPixelColor(STROBEPIX[j], strip.Color(STB_C[0], STB_C[1], STB_C[2]));
+      strip.setPixelColor(STROBEPIX[j], STB_COLOR);
     }
     strip.show();
-    delay(50);
+    delay(STB_DEL);
     
     for (j = 0; j < SPIXNUM; j++) { // loop through all strobed pins and return to original color
       strip.setPixelColor(STROBEPIX[j], strip.Color(g[STROBEPIX[j]], r[STROBEPIX[j]], b[STROBEPIX[j]]));
     }
     strip.show();
-    delay(50);
+    delay(STB_DEL);
   }
 
 
